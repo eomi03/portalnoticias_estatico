9c3c8734d533ba19c879730ea76e84f53bc6c2c3
Miguel Cirico <miguel.ciroc03@gmail.com>
2024-10-09 13:07:39 -0300

    PROJETO

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-const bodyParser = require('body-parser');
 const express = require('express');
-
+var bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
-}))
+}));
 
 app.engine('html', require('ejs').renderFile);
-app.set('view engine','html');
-app.use('/public',express.static(path.join(__dirname, 'public')));
-app.set('views',path.join(__dirname,'/views'));
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/pages'));
 
-
-app.get('/',(req,res)=>{
-    console.log(req.query);
-
-    if (!req.query.busca) {
-        res.send('HOME');
+app.get('/', (req, res) => {
+    if (req.query.busca == null) {
+        res.render('home', {});
     } else {
         res.send('Você buscou: ' + req.query.busca);
     }
 });
 
-
-app.get('/:slug',(req,res)=>{
+app.get('/:slug', (req, res) => {
     res.send(req.params.slug);
-})
+});
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('server rodando!');
-})
+});
